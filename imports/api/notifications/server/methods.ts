d1701a231cef80d805043768cc5cd28cfae40319
Jesse Bellingham <jesse.bellingham@xero.com>
2020-09-06 16:30:09 +1000

    Nodemailer service for local dev

--- imports/api/notifications/server/methods.ts
+++ imports/api/notifications/server/methods.ts
@@ -1,11 +1,14 @@
 import { Meteor } from 'meteor/meteor'
 import { SendGridNotificationService } from '../../../../server/service/notifications/SendgridNotificationService'
+import { NodemailerNotificationService } from '../../../../server/service/notifications/NodemailerNotificationService'
 
 export const notifications = {
     sendNotification: 'sendNotification'
 }
 
-const emailService = new SendGridNotificationService()
+const emailService = Meteor.isDevelopment ?
+    new NodemailerNotificationService() :
+    new SendGridNotificationService()
 
 Meteor.methods({
     async sendNotification() {
--- server/service/notifications/NodemailerNotificationService.ts
+++ server/service/notifications/NodemailerNotificationService.ts
@@ -1,20 +1,29 @@
 import * as nodemailer from 'nodemailer'
 import { INotificationService } from "./INotificationService";
 
-class NodemailerNotificationService implements INotificationService {
+export class NodemailerNotificationService implements INotificationService {
     private transporter
-    constructor() {
+    
+    async sendEmail() {
+        let testAccount = await nodemailer.createTestAccount();
         this.transporter = nodemailer.createTransport({
-            host: "smtp.ethereal.email",
-            port: 587,
-            secure: false, // true for 465, false for other ports
-            // auth: {
-            //   user: testAccount.user, // generated ethereal user
-            //   pass: testAccount.pass, // generated ethereal password
-            // },
-          });
+            host: "localhost",
+            port: 1025,
+            ignoreTLS: true,
+        });
+        console.log('sending test email')
 
+        // send mail with defined transport object
+        let info = await this.transporter.sendMail({
+            from: '"Fred Foo 👻" <foo@example.com>', // sender address
+            to: "bar@example.com, baz@example.com", // list of receivers
+            subject: "Hello ✔", // Subject line
+            text: "Hello world?", // plain text body
+            html: "<b>Hello world?</b>", // html body
+        });
+
+        console.log("Message sent: %s", info.messageId);
+        // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+        return true
     }
-    sendEmail: () => Promise<boolean>;
-
 }
